c49942a2dda3f648067db8379f6fd7048bbc2266
ViKMacedo <64294657+ViKMacedo@users.noreply.github.com>
2021-03-19 01:57:45 -0300

    Add files via upload

--- MyApiWithDoc/Startup.cs
+++ MyApiWithDoc/Startup.cs
@@ -1,17 +1,22 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
+using API;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace MyApiWithDoc
 {
     public class Startup
     {
+
         private static string GetPathOfXmlFromAssembly() => Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
 
         public Startup(IConfiguration configuration)
@@ -38,6 +43,33 @@ namespace MyApiWithDoc
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API with Documentation", Version = "v1" });
                 options.IncludeXmlComments(GetPathOfXmlFromAssembly());
             });
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
+            services.AddCors();
+            services.AddControllers();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +84,7 @@ namespace MyApiWithDoc
 
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API width Documentation V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API with Documentation V1");
                 // c.RoutePrefix = string.Empty;
             });
 
@@ -67,9 +99,16 @@ namespace MyApiWithDoc
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
+
+            app.UseAuthentication());
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
+
     }
 }
